74ca4f8c5cdd7227a0d4dd2f8975ded132108ecc
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-15 19:06:40 -0300

    Added Enviroment Variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,6 @@ import ReactDOM from "react-dom/client";
 import CalendarApp from "./CalendarApp";
 import "./index.css";
 
+console.log(import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById("root")).render(<CalendarApp />);
