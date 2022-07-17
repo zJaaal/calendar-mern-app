c95fb4540377bb05d25fb672f4a1830a5928862e
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-17 19:14:58 -0300

    Added types for event actions

--- src/types/types.js
+++ src/types/types.js
@@ -3,10 +3,13 @@ export const types = {
   uiCloseModal: "[ui] Modal closed",
 
   eventAddNew: "[event] Added new event",
+  eventStartAddNew: "[event] Add new event Started",
   eventSetActive: "[event] Setted active event",
   eventCleanActive: "[event] Cleaned active event",
   eventEditActive: "[event] Edited active event",
   eventDeleteActive: "[event] Deleted active event",
+  eventLoaded: "[event] Events has been loaded",
+  eventLogout: "[event] Event state has been deleted",
 
   authChecking: "[auth] Checking login state",
   authCheckingFinished: "[auth] Checking login state finished",
