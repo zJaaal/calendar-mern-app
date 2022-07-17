4d4205dc8a372f332d4631cfec502ae5781bb2ce
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-16 23:33:53 -0300

    Now you can log out

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,14 +1,27 @@
 import { Box, AppBar, Toolbar, Typography, Button } from "@mui/material";
 import LogoutIcon from "@mui/icons-material/Logout";
+import { useDispatch, useSelector } from "react-redux";
+import { startLogout } from "../../actions/auth";
 const NavBar = () => {
+  const { name } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(startLogout());
+  };
   return (
     <Box flexGrow={1}>
       <AppBar position="static" color="primary">
         <Toolbar>
           <Typography variant="h6" component={"div"} flexGrow={1}>
-            Jalinson
+            {name}
           </Typography>
-          <Button variant="text" color="inherit" endIcon={<LogoutIcon />}>
+          <Button
+            variant="text"
+            color="inherit"
+            endIcon={<LogoutIcon />}
+            onClick={handleLogout}
+          >
             Log out
           </Button>
         </Toolbar>
