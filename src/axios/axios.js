2e6737a5e9348a73874cc5bf26a24ea228694564
Jalinson Diaz <zjaaaldev@gmail.com>
2022-07-17 19:15:44 -0300

    Added axios requests to api

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -25,3 +25,36 @@ export const registerUser = (name, email, password) => {
 export const renewToken = (token) => {
   return instance.get("/auth/renew", { headers: { "x-token": token } });
 };
+
+//Events Endpoints
+
+//Create event
+export const createEvent = (event, token) => {
+  return instance.post(
+    "/events",
+    { ...event },
+    { headers: { "x-token": token } }
+  );
+};
+
+//Get all events
+export const getEvents = (token) => {
+  return instance.get("/events", { headers: { "x-token": token } });
+};
+
+//Update Event
+export const updateEvent = (token, event, eventId) => {
+  return instance.put(
+    `/events/${eventId}`,
+    { ...event },
+    {
+      headers: { "x-token": token },
+    }
+  );
+};
+//Delete event
+export const deleteEvent = (token, eventId) => {
+  return instance.delete(`/events/${eventId}`, {
+    headers: { "x-token": token },
+  });
+};
